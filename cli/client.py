7c46b56cad5a9fab60cf1a1f31792c7362b2ba2b
Jonathan Liu <jonliu168@gmail.com>
2025-08-18 19:01:25 -0700

    FSM and please make dictionaries and remove the if's

--- cli/client.py
+++ cli/client.py
@@ -10,9 +10,17 @@
 import click
 import socketio
 import os
-import signal
+from enum import Enum
 
 localCache = []
+
+class FSM(Enum):
+    BOOT=0
+    CHANNEL=1
+    SIGNUP=2
+    LOGIN=3
+
+fsmState = FSM.BOOT
 
 sio = socketio.Client()
 sio.connect('http://127.0.0.1:6221')
@@ -23,6 +31,9 @@ isChatActive = False
 @sio.on('response')
 def response(data):
   localCache.append(data)
+  click.clear()
+  draw()
+  draw()
 
 @click.group(invoke_without_command=True)
 @click.option("--command", prompt=">")
@@ -48,6 +59,8 @@ def clear():
    draw()
 
 def draw():
+    global fsmState
+    global username
     click.clear()
     cols = os.get_terminal_size().columns
     rows = os.get_terminal_size().lines
@@ -63,17 +76,39 @@ def draw():
     print(title)
     click.echo(line)
 
+    if(fsmState == FSM.BOOT):
+        prompt = click.prompt("Please boot into an option: \n 1: Signup \n 2: Login \n 3: Join")
+        if(prompt == "1"):
+            fsmState = FSM.SIGNUP
+            draw()
+        if(prompt == "2"):
+            fsmState = FSM.LOGIN
+        if(prompt == "3"):
+            fsmState = FSM.CHANNEL
+        return
+    if(fsmState == FSM.SIGNUP):
+        username = click.prompt("Enter a username")
+        fsmState = FSM.CHANNEL
+        draw()
+        return
+    
+
+
+    
+        
+    
+
     for message in localCache:
         click.echo(f"{message['from']} says: \n {message['message']}")
 
     #DRAW THE RIGHT SIDE UI
     # Header at the right edge
     print(f"\033[{4};{mainWindowWidth}H| Online Users")
-    print(f"\033[{mainWindowWidth}G|" + "-" * (cols-mainWindowWidth))
+    print(f"\033[{5};{mainWindowWidth}H|{'-' * (cols-mainWindowWidth)}")
+    # print(f"\033[{mainWindowWidth}G|" + "-" * (cols-mainWindowWidth))
 
-    # Draw vertical bar
-    for _ in range(rows - 6):
-        print(f"\033[{mainWindowWidth}G|")
+    for _ in range(6,rows):
+        print(f"\033[{_};{mainWindowWidth}H|")
         
 
 @cli.command(name='q')
@@ -84,9 +119,9 @@ def quitapp():
 def client():
     draw()   
     global isChatActive 
+    global username
     while True:
         try:
-            draw()
             user_input = click.prompt("> ", prompt_suffix="")
 
             if user_input.lower() == "chat":
@@ -95,7 +130,7 @@ def client():
                 continue
 
             if isChatActive:
-                sio.emit("message", {"from": "skib", "message": str(user_input)})
+                sio.emit("message", {"from": username, "message": str(user_input)})
             else:
                 cmd = user_input.lower()
                 if cmd == "q" or cmd == "quit":
