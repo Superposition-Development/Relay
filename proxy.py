0bc6df3f4c4dce68d7736f32d5c4c984a3196a80
Jonathan Liu <jonliu168@gmail.com>
2026-02-13 17:46:30 -0800

    every end of a time is another begun

--- proxy.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-proxies = {
-  'http': 'http://localhost:7777',
-  'https': 'http://localhost:7777',
-}
-
-response = requests.get('http://yt-rpc.onrender.com/', proxies=proxies)
-print(response.text)
