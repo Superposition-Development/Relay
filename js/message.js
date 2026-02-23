62b9be8ff9cbc91c65a99c41264e48fdc75b5c48
Jonathan Liu <jonliu168@gmail.com>
2026-02-23 10:54:38 -0800

    get server endpoint but it needs to do something with the data

--- js/message.js
+++ js/message.js
@@ -81,3 +81,26 @@ function createServer() {
             console.error("There was a problem with the fetch", error);
         });
 }
+
+function getServers() {
+    let JWTCookie = getCookie("RelayJWT")
+    fetch(serverAddress + getServerEndpoint,
+        {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                'Authorization': `Bearer ${JWTCookie}`,
+            },
+            credentials:"include"
+        }).then(response => {
+            if (response.ok) {
+                return response.json()
+            }
+            throw new Error("Network response failed")
+        }).then(data => {
+            console.log(data)
+        })
+        .catch(error => {
+            console.error("There was a problem with the fetch", error);
+        });
+}
