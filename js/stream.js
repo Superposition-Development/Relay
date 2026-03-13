ae734cd88836c8d4d6a1540886c3e22728cbca30
Jonathan Liu <jonliu168@gmail.com>
2026-03-13 13:56:21 -0700

    start of WebRTC calling system

---
+++ js/stream.js
@@ -0,0 +1,23 @@
+const stunServers = { iceServers: [{ url: 'stun:stun.l.google.com:19302' }] }
+let localConnection;
+
+async function joinCall()
+{
+    const connAttempt = new RTCPeerConnection(stunServers)
+    connAttempt.addTransceiver("video")
+    connAttempt.addTransceiver("audio")
+    const localOffer = await connAttempt.createOffer()
+    await connAttempt.setLocalDescription(localOffer)
+
+    connAttempt.onicecandidate = (e) => {
+        if (e.candidate)
+        {
+            //send smth to the SFU
+        }
+    }
+    localConnection = connAttempt
+    //send smethh to the SFU via localOffer.sdp
+}
+
+//brb, reminder ot add smth abt globalPeer.addIceCandidate(new RTCIceCandidate())
+//make the websocket fire an event and add event listeners across the files 
