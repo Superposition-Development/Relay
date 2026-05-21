2c6e5f238d325ee971530f6f8c23e6892460e90f
Jonathan Liu <jonliu168@gmail.com>
2026-05-21 09:14:56 -0700

    rename to const.js

--- js/init.js
+++ js/init.js
@@ -1,16 +1,1 @@
-let serverAddress = "http://127.0.0.1:8080"
-
-let signupEndpoint = "/signup"
-let loginEndpoint = "/login"
-
-let createServerEndpoint = "/createServer"
-let getServerEndpoint = "/getServers"
-let joinServerEndpoint = "/joinServer"
-
-let createChannelEndpoint = "/createChannel"
-let getChannelEndpoint = "/getChannels"
-
-//socket endpoints
-let sendMessageEndpoint = "/sendMessage"
-let getMessagesEndpoint = "/getMessages"
-
+//stuff like run getServers and eerything that should happen on startup
