98443d085292e6ec28392fe2890c739ab544de25
Jonathan Liu <jonliu168@gmail.com>
2026-05-23 14:35:24 -0700

    root, DOM, queryParams

--- js/DOM.js
+++ js/DOM.js
@@ -1,3 +1,20 @@
+function navigate(params) {
+    const url = new URL(window.location);
+
+    Object.keys(params).forEach(key => {
+        url.searchParams.set(key, params[key]);
+    });
+
+    history.replaceState({}, "", url);
+    router();
+}
+
+function router() {
+    const hash = window.location.hash.slice(1);
+
+    const parts = hash.split("/");
+    //parts[0...n]
+}
 function CreateServerDOM(id,name,pfp)
 {
     let serverBrowser = document.getElementById("serverBrowser")
@@ -7,7 +24,7 @@ function CreateServerDOM(id,name,pfp)
     serverNode.dataset.name = name
     serverNode.dataset.serverID = id
     serverNode.addEventListener("click", function(){
-                 navigate(`/server/${id}`)
+                 navigate({"serverID":id})
              });
     serverBrowser.appendChild(serverNode)
 }
--- js/root.js
+++ js/root.js
@@ -1,9 +1,15 @@
 async function boot()
 {
+    const params = new URLSearchParams(window.location.search);
+    const serverID = params.get("serverID"); // "John"
+    const channelID = params.get("channelID"); // "30"
+
     initServers = await getServers()
     for (const server of initServers.data) {
         CreateServerDOM(server.id,server.name,server.pfp)
     }
+
+
 }
 
 boot()
