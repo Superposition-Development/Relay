31dd04388f53cb82b54bf32b78b779c543a6b109
Jonathan Liu <jonliu168@gmail.com>
2026-05-30 11:17:32 -0700

    more divs

--- main.go
+++ main.go
@@ -9,8 +9,16 @@ import (
 
 var (
 	HeaderStyle = lipgloss.NewStyle().
-		Background(lipgloss.Color("#444")).
-		Foreground(lipgloss.Color("#FFF"))
+			Background(lipgloss.Color("#444")).
+			Foreground(lipgloss.Color("#FFF"))
+
+	ServerBarStyle = lipgloss.NewStyle().
+			Background(lipgloss.Color("#121212")).
+			Width(10)
+
+	channelBarStyle = lipgloss.NewStyle().
+			Background(lipgloss.Color("#1E1E1E")).
+			Width(28)
 )
 
 type model struct {
@@ -50,13 +58,89 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 func (m model) View() string {
 	header := HeaderStyle.Width(m.width).Render("Relay")
 
-	body := "Main content here"
+	serverBody := m.renderServerBar()
+	channelBody := m.renderChannelBar()
+	body := lipgloss.JoinHorizontal(
+		lipgloss.Top,
+		serverBody,
+		channelBody,
+	)
 
 	return lipgloss.JoinVertical(
 		lipgloss.Left,
 		header,
 		body,
 	)
+}
+
+func (m model) renderServerBar() string {
+	// var out strings.Builder
+
+	// out.WriteString("\n")
+
+	// out.WriteString(
+	// 	lipgloss.NewStyle().
+	// 		Foreground(lipgloss.Color("#FFFFFF")).
+	// 		Bold(true).
+	// 		Render(" SRD "),
+	// )
+
+	// out.WriteString("\n\n")
+
+	// out.WriteString("10A 250VAC\n")
+	// out.WriteString("10A 125VAC\n")
+	// out.WriteString("10A  30VDC\n")
+	// out.WriteString("SRD-05VDC\n")
+
+	// out.WriteString("\n")
+
+	// for _, s := range m.servers {
+	// 	box := lipgloss.NewStyle().
+	// 		Width(6).
+	// 		Height(3).
+	// 		Align(lipgloss.Center, lipgloss.Center).
+	// 		Background(lipgloss.Color("#2F6DB2")).
+	// 		Foreground(lipgloss.Color("#FFFFFF")).
+	// 		MarginTop(1).
+	// 		Render(s.Name)
+
+	// 	out.WriteString(box)
+	// 	out.WriteString("\n")
+	// }
+
+	return ServerBarStyle.
+		Height(m.height - 1).
+		Render()
+}
+
+func (m model) renderChannelBar() string {
+	// var out strings.Builder
+
+	// serverName := serverNameStyle.
+	// 	Width(28).
+	// 	Render("ServerName of infinite doom")
+
+	// out.WriteString(serverName)
+	// out.WriteString("\n")
+
+	// for i, ch := range m.channels {
+	// 	style := channelStyle
+
+	// 	if i == m.selectedChannel {
+	// 		style = selectedChannelStyle
+	// 	}
+
+	// 	out.WriteString(
+	// 		style.Width(28).Render("# " + ch.Name),
+	// 	)
+
+	// 	out.WriteString("\n")
+	// }
+
+	return channelBarStyle.
+		Height(m.height - 1).
+		// Render(out.String())
+		Render()
 }
 
 func main() {
