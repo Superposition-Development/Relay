cec8e93aa7893908c36ec4e6b40e09371aa3a4cd
Jonathan Liu <jonliu168@gmail.com>
2026-06-11 09:27:59 -0700

    modal.js

---
+++ js/modal.js
@@ -0,0 +1,11 @@
+function createModal()
+{
+    document.getElementById("modalWrapper").style.display = "flex"
+    
+}
+
+function closeModal()
+{
+    document.getElementById("modalWrapper").style.display = "none"
+    document.getElementById("modalContent").innerHTML = ""
+}
