915d468d10e10abdfee820dc0f0916292aa68b5c
Jonathan Liu <jonliu168@gmail.com>
2026-06-18 19:28:27 -0700

    gonna eat, fixing modal later

--- js/modal.js
+++ js/modal.js
@@ -1,11 +1,14 @@
-function createModal()
-{
+function createModal() {
+    let modalWrapper = document.getElementById("modalWrapper")
     document.getElementById("modalWrapper").style.display = "flex"
-    
+    modalWrapper.style.filter = "opacity(100%)"
 }
 
-function closeModal()
-{
-    document.getElementById("modalWrapper").style.display = "none"
-    document.getElementById("modalContent").innerHTML = ""
+function closeModal() {
+    let modalWrapper = document.getElementById("modalWrapper")
+    modalWrapper.style.filter = "opacity(0%)"
+    window.setTimeout(function () {
+        modalWrapper.style.display = "none"
+        modalWrapper.innerHTML = ""
+    }, 1000)
 }
