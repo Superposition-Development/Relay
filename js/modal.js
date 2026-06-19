62841dc6a83e978ac88c19eba431337ce0494c89
Jonathan Liu <jonliu168@gmail.com>
2026-06-19 00:00:20 -0700

    dynamic create modal

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,41 @@
-function createModal() {
-    let modalWrapper = document.getElementById("modalWrapper")
-    document.getElementById("modalWrapper").style.display = "flex"
-    modalWrapper.style.filter = "opacity(100%)"
+function setModalHeader(title)
+{
+    document.getElementById("modalTitle").innerText = title
 }
+
+function createModalPictureUpload()
+{
+
+}
+
+function createServerModal()
+{
+    createModal(() => setModalHeader("title"),createModalPictureUpload,)
+}
+
+function createModal(...modules) {
+    let modalWrapper = document.getElementById("modalWrapper")
+    modalWrapper.style.filter = "opacity(0%)"
+    document.getElementById("modalWrapper").style.display = "flex"
+    window.setTimeout(function () {
+        modalWrapper.style.filter = "opacity(100%)"
+    }, 20)
+
+    for (let module of modules) {
+        module();
+    }
+}
+
+/*createModal(
+    () => setModalHeader("title")
+);
+*/
 
 function closeModal() {
     let modalWrapper = document.getElementById("modalWrapper")
     modalWrapper.style.filter = "opacity(0%)"
     window.setTimeout(function () {
         modalWrapper.style.display = "none"
-        modalWrapper.innerHTML = ""
+        document.getElementById("modalContent").innerHTML = ""
     }, 1000)
 }
